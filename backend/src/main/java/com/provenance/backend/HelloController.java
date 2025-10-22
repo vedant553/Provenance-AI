468c1dfd0a6a88110656b16d8893798786501fa1
Vedant Nandeshwar <vedantnandeshwar2003@gmail.com>
2025-10-22 16:33:03 +0530

    Initial commit

---
+++ backend/src/main/java/com/provenance/backend/HelloController.java
@@ -0,0 +1,12 @@
+package com.provenance.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Backend is working!";
+    }
+}
